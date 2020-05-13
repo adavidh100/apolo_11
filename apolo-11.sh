02122e6f6b45a022503a6d507dc723987f6becb8
carlosalbertobarrera <64572823+carlosalbertobarrera@users.noreply.github.com>
2020-05-12 23:51:05 -0500

    Modificacion incluyendo config.ini y movimiento de logs

--- apolo-11.sh
+++ apolo-11.sh
@@ -11,19 +11,140 @@ fn_crear_archivo(){
 	vRutaArchivo=$1
 	vContenidoArchivo=$2
 	vNombreArchivo=$3
+    fecha=$(date +%d%m%y)
+    hora=`date +"%H:%M"`
 
-	echo "Se crea el archivo : "$vNombreArchivo" en la ruta "$vRutaArchivo/$vNombreArchivo
+    texto="Se crea el archivo de log : "$vNombreArchivo" a las  "$hora 
+    file="log"$fecha".log"
+    archivoGlobalLog=$vRutaArchivo/$file
+    echo "voy a escribir" $texto "en " $archivoGlobalLog
+
+     
+    sudo touch $archivoGlobalLog
+    sudo chmod 777 $archivoGlobalLog
+    sudo echo $texto >> $archivoGlobalLog
+	
     sudo touch $vRutaArchivo/$vNombreArchivo
-    sudo chown :$nombreGrupo $vRutaArchivo/$vNombreArchivo
+    sudo chown :$nombreGrupo $vRutaArchivo/$vNsombreArchivo
 	sudo chown ${siglaProyecto[$indexSiglaProyecto]} $vRutaArchivo/$vNombreArchivo
     sudo chmod 777 $vRutaArchivo/$vNombreArchivo
 	sudo echo $vContenidoArchivo > $vRutaArchivo/$vNombreArchivo
 	
 }
+#Funcion para crear el archivo de configuracion completo en caso de que este no exista.
+fn_crear_configIni(){
+echo "#Directorio principal
+directorioRaiz=\"/cloudera/takeoff-mission\"
 
+#Variable que contiene las siglas del proyecto del programa apolo-11
+#
+# Orbit(ORB)             : modernizar toda la flota de satélites para potenciarlos y mejorar su alcance de cobertura y comunicaciones y 
+# Colony(CNL)           : proyecto para crear una colonia lunar 
+# Mars(MRS)             : proyecto para llevar personas de turismo a marte 
+# Desconocido(DES) : Control para dispositivo no controlado o infiltrado 
+siglaProyecto=(\"ORB\" \"CNL\" \"MRL\" \"DES\")
+
+# Diferentes opciones que puede contener el log generado
+opcionesArchivo=(\"unknow\" \"excelent\" \"good\" \"warning\" \"kill\")
+
+# Subcarpetas donde deben quedar almacenados los log generados por los diferentes dispositivos
+subcarpetas=(\"devices\" \"logsapolo11\" \"config\" \"stats\")
+
+#Cantidad de archivos que genera el programa apolo-11 por ejecucion
+numeroArchivosGenera=2
+
+#Cantidad maxima de archivos que genera el programa apolo-11 por dia
+maximoArchivosGenera=1000" >>$vArchivo
+
+}
+#funcion que inspecciona todas las variables sobre un archivo config.ini ya existente.
+fn_inspeccionarConfigIni(){
+
+param=$1
+echo "Validando" "${!param}"
+if [[ -z "${!param}" ]]; 
+then
+    echo "La variable " $param " no existe, procedere a escribirla en el archivo"    
+    
+    case $1 in
+     directorioRaiz)
+    echo "
+    
+#Directorio principal
+directorioRaiz=\"/cloudera/takeoff-mission\"">>$vArchivo
+     ;;
+
+     siglaProyecto)
+    echo "    
+#Variable que contiene las siglas del proyecto del programa apolo-11
+#
+# Orbit(ORB)             : modernizar toda la flota de satélites para potenciarlos y mejorar su alcance de cobertura y comunicaciones y 
+# Colony(CNL)           : proyecto para crear una colonia lunar 
+# Mars(MRS)             : proyecto para llevar personas de turismo a marte 
+# Desconocido(DES) : Control para dispositivo no controlado o infiltrado 
+siglaProyecto=(\"ORB\" \"CNL\" \"MRL\" \"DES\")">>$vArchivo
+     ;;
+
+     opcionesArchivo)
+    echo "
+    
+# Diferentes opciones que puede contener el log generado
+opcionesArchivo=(\"unknow\" \"excelent\" \"good\" \"warning\" \"kill\")">>$vArchivo
+     ;;
+     
+    subcarpetas)
+    echo "
+    
+# Subcarpetas donde deben quedar almacenados los log generados por los diferentes dispositivos
+subcarpetas=(\"devices\" \"logsapolo11\" \"config\" \"stats\")">>$vArchivo
+     ;;
+
+    numeroArchivosGenera)
+    echo "
+    
+#Cantidad de archivos que genera el programa apolo-11 por ejecucion
+numeroArchivosGenera=2">>$vArchivo
+     ;;
+
+     maximoArchivosGenera)
+    echo "
+    
+#Cantidad maxima de archivos que genera el programa apolo-11 por dia
+maximoArchivosGenera=1000">>$vArchivo
+     ;;
+     
+    esac
+else    
+echo "La variable " $1 " existe, todo tranqui" 
+fi
+} 
+#Funcion que valida si el archivo existe y si existe, invoca inspeccionar con las variables requeridas por el sistema.
+fn_valida_configIni(){
+
+	echo "Validando si existe" $1
+
+	vArchivo=$1
+
+	if [ -f $vArchivo ]; then
+        source $vArchivo
+		echo "El archivo config.ini existe"
+        fn_inspeccionarConfigIni directorioRaiz
+        fn_inspeccionarConfigIni siglaProyecto
+        fn_inspeccionarConfigIni opcionesArchivo
+        fn_inspeccionarConfigIni subcarpetas
+        fn_inspeccionarConfigIni numeroArchivosGenera	
+        fn_inspeccionarConfigIni maximoArchivosGenera  
+	else 
+		fn_crear_configIni $vArchivo
+        source $vArchivo
+        echo "Se creara el archivo $vArchivo [OK]"
+        
+	fi
+}
+#Funcion que revisa si existe un directorio y encaso de no existencia, lo crea.
 fn_valida_directorio(){
 
-	echo "Ruta enviada a la funcion fn_valida_directorio : "$1
+	echo "Ruta enviada a fn_valida_directorio : "$1
 
 	vRuta=$1
 
@@ -34,7 +155,7 @@ fn_valida_directorio(){
         echo "Se creara la ruta $vRuta [OK]"
 	fi
 }
-
+#Funcion que imprime Menu
 fn_menu(){
     
     echo "**************************************************************************"
@@ -47,18 +168,20 @@ fn_menu(){
     echo "*                 Daniel Esteban Lopez                                   *"
     echo "*                 Andres David Herrera Parrado                           *"
     echo "**************************************************************************"
-    echo -e "Digite una opcion : \n\n 1. Ejecutar apolo-11 \n 2. Salir"
-    echo
-    read -p "Digite opcion deseada: " opcion
 }
 
+#Inicializacion de programa
 fn_inicializar(){
 
     PATH2=$(pwd)
-    echo $PATH2
-    source $PATH2/config.ini
-	directorioAValidar=$(echo ~ 2>&1)$directorioRaiz
-    echo $directorioAValidar
+    echo "el path actual es "$PATH2
+#valida si existe config.ini y si no existe lo crea
+    fn_valida_configIni $PATH2/config.ini
+    echo "validacion de config ini terminada"
+    #directorioAValidar=$(echo ~ 2>&1)$directorioRaiz
+   #valida que el directorio Raiz exista 
+    directorioAValidar=$directorioRaiz
+    echo "el directorio a validar es" $directorioAValidar
 
 # Se valida el directorio Ppal y las subcarpetas
 	fn_valida_directorio $directorioAValidar
@@ -68,31 +191,47 @@ fn_inicializar(){
 		fn_valida_directorio $subCarpetasAValidar
 	done
 # Se valida si existe la carpeta con la fecha de ejecucion de apolo-11
-    fecha=$(date +%d-%m-%y)
-	ruta=$directorioRaiz/${subcarpetas[0]}/$fecha
+    fecha=$(date +%d%m%y)
+	ruta=$directorioRaiz/${subcarpetas[0]}/$fecha  
     fn_valida_directorio $ruta
+
+# Se valida si existe la carpeta de logs con la fecha de ejecucion de apolo-11
+    
+    rutaOrigen=$directorioRaiz/${subcarpetas[0]}/$fecha/
+    rutaDestino=$directorioRaiz/logsapolo11/bkmissions/$fecha/
+    fn_valida_directorio $rutaOrigen
+    fn_valida_directorio $rutaDestino
+    echo "Ruta Origen" $rutaOrigen
+    echo "RutaDestino" $rutaDestino 
+   
+    cd $rutaDestino
+    let contadorBkMissions=$(ls | wc -l)
+
     cd $ruta
-    let contador=$(ls | wc -l)+1
-    let finalCiclo=$(ls | wc -l)+$numeroArchivosGenera
+    let contadorTmp=$(ls | wc -l) +1
+
+    let contador=$contadorBkMissions+contadorTmp
+
+    
+    let finalCiclo=($contador+$numeroArchivosGenera)-1
     cd $PATH2
 
 }
+clear
 #Ejecutamos el procedimiento inicializar para garantizar el arbol del proyecto
+
 fn_inicializar
 
 #Mostramos el menu y capturamos el valor deseado
 fn_menu
 
-while [ $opcion -ne 2 ]; do
-    case $opcion in 
-    1) 
-        echo
-        echo "Se inicia operacion apolo-11.............."
-        echo
-        ruta=$directorioRaiz
-        contadorArchivos=$contador
-        nombreGrupo="nasa"
-        sudo groupadd $nombreGrupo 2>/dev/null
+    echo
+    echo "Se inicia operacion apolo-11.............."
+    echo
+    ruta=$directorioRaiz
+    contadorArchivos=$contador
+    nombreGrupo="nasa"
+    sudo groupadd $nombreGrupo 2>/dev/null
 # generador de numeros aleatorios entre 0 - 4 echo $(($RANDOM%4))
         if [ $contadorArchivos -le $maximoArchivosGenera ]; then
             while [ $contadorArchivos -le $finalCiclo ]; do
@@ -116,25 +255,26 @@ while [ $opcion -ne 2 ]; do
         echo
         echo "Se finaliza operacion apolo-11.............."
         echo
-    ;;
-    *)
-        echo
-        echo "Opcion invalida!!!!"    
-    esac
+    
+    echo "Moviendo logs..."
+
+    # Se valida si existe la carpeta con la fecha de ejecucion de apolo-11
+    fecha=$(date +%d%m%y)
+	rutaOrigen=$directorioRaiz/${subcarpetas[0]}/$fecha/
+    rutaDestino=$directorioRaiz/logsapolo11/bkmissions/$fecha/
+    fn_valida_directorio $rutaOrigen
+    fn_valida_directorio $rutaDestino
+    echo "Ruta Origen" $rutaOrigen
+    echo "RutaDestino" $rutaDestino 
+   
+    
+    cd $rutaOrigen
+    sudo mv APL*.log $rutaDestino 
+    cd $directorioRaiz
+        
+    echo "Se movieron los archivos satisfactoriamente!"
+    
+    
+    echo ""
+    echo "Finalizando sesion en mision apolo-11 !!!"
     echo
-    read -p "Desea Continuar S/N: " continuar
-
-    if [ $continuar == "S" ] | [ $continuar == "s" ]; then
-        fn_inicializar
-        fn_menu
-    else
-        opcion=2
-    fi
-done
-
-if [ $opcion -eq 2 ]; then
-        clear
-        echo ""
-        echo "Finalizando sesion en mision apolo-11 !!!"
-        echo
-fi
